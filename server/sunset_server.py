309a1ae077314f889436b3a8b46d096fcec18ed3
VoidJackLee <1071426378@163.com>
2025-05-22 19:33:45 +0800

    add sunrise

--- server/sunset_server.py
+++ server/sunset_server.py
@@ -5,9 +5,9 @@ from geopy.distance import distance
 import time
 import math
 
-from src.degree import get_sunset
+from src.degree import get_sunset, get_sunrise
 
-def getPolyLine(lat,lng,time=None):
+def getSunsetPolyLine(lat,lng,time=None):
     time_sunset,deg_dir,deg_height = get_sunset(lat,lng,time)
     _,before_deg_dir,before_deg_height = get_sunset(lat,lng,time,delta=timedelta(hours=-0.5))
     _,after_deg_dir,adter_deg_height = get_sunset(lat,lng,time,delta=timedelta(hours=0.5))
@@ -20,7 +20,7 @@ def getPolyLine(lat,lng,time=None):
     after_mid_lat, after_mid_lng, _ = distance(kilometers=200).destination((lat,lng),bearing=after_deg_dir * 180 / math.pi)
 
     data = {
-        "sunset": [
+        "target": [
             [lat,lng], # 原点
             [mid_lat, mid_lng], # 落日200km点
             [target_lat, target_lng], # 落日400km点
@@ -35,7 +35,39 @@ def getPolyLine(lat,lng,time=None):
             [after_mid_lat, after_mid_lng], # 落日后半小时200km点
             [after_target_lat, after_target_lng], # 落日后半小时400km点
         ],
-        "time_sunset": time_sunset
+        "time": time_sunset
+    }
+    return data
+
+def getSunrisePolyLine(lat,lng,time=None):
+    time_sunrise,deg_dir,deg_height = get_sunrise(lat,lng,time)
+    _,before_deg_dir,before_deg_height = get_sunrise(lat,lng,time,delta=timedelta(hours=-0.5))
+    _,after_deg_dir,adter_deg_height = get_sunrise(lat,lng,time,delta=timedelta(hours=0.5))
+
+    target_lat, target_lng, _ = distance(kilometers=400).destination((lat,lng),bearing=deg_dir * 180 / math.pi)
+    mid_lat, mid_lng, _ = distance(kilometers=200).destination((lat,lng),bearing=deg_dir * 180 / math.pi)
+    before_target_lat, before_target_lng, _ = distance(kilometers=400).destination((lat,lng),bearing=before_deg_dir * 180 / math.pi)
+    before_mid_lat, before_mid_lng, _ = distance(kilometers=200).destination((lat,lng),bearing=before_deg_dir * 180 / math.pi)
+    after_target_lat, after_target_lng, _ = distance(kilometers=400).destination((lat,lng),bearing=after_deg_dir * 180 / math.pi)
+    after_mid_lat, after_mid_lng, _ = distance(kilometers=200).destination((lat,lng),bearing=after_deg_dir * 180 / math.pi)
+
+    data = {
+        "target": [
+            [lat,lng], # 原点
+            [mid_lat, mid_lng], # 日出200km点
+            [target_lat, target_lng], # 日出400km点
+        ],
+        "before30": [
+            [lat,lng], # 原点
+            [before_mid_lat, before_mid_lng], # 日出前半小时200km点
+            [before_target_lat, before_target_lng], # 日出前半小时400km点
+        ],
+        "after30": [
+            [lat,lng], # 原点
+            [after_mid_lat, after_mid_lng], # 日出后半小时200km点
+            [after_target_lat, after_target_lng], # 日出后半小时400km点
+        ],
+        "time": time_sunrise
     }
     return data
 
@@ -58,4 +90,8 @@ base = 'sunset'
 
 @app.get(f"/{base}/api/getSunsetTime")
 async def getSunsetTime(lat: float, lng: float):
-    return ok(getPolyLine(lat, lng))
+    return ok(getSunsetPolyLine(lat, lng))
+
+@app.get(f"/{base}/api/getSunriseTime")
+async def getSunriseTime(lat: float, lng: float):
+    return ok(getSunrisePolyLine(lat, lng))
