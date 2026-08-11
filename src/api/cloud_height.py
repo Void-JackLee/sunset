19d357f84c5a2858f7a40869e20c71b03a78418c
VoidJackLee <1071426378@163.com>
2026-08-11 22:19:52 +0800

    add: timezone and mainCloudBottom in /point_cloud_forecast

--- src/api/cloud_height.py
+++ src/api/cloud_height.py
@@ -1,13 +1,20 @@
+from datetime import datetime
+
 from fastapi import APIRouter, Request
 
-from ..service.open_meteo import get_point_forecast
+from ..service.open_meteo import get_point_forecast, find_main_cloud_bottom
 from ..result import ok, err
 
 app = APIRouter()
 
-@app.get("/point_cloud_height")
-def point_cloud_height(lat: float, lng: float, time: str, model: str = "gfs_global", timezone: int = 8):
+@app.get("/point_cloud_forecast")
+def point_cloud_forecast(lat: float, lng: float, time: str, model: str = "gfs_global", main_cloud_rate_to_max_cc: float =0.4, skip_low_clouds_threshold: float = 0.3):
+    dt = datetime.fromisoformat(time)
+    timezone = int(dt.utcoffset().total_seconds() / 3600)
+    time = dt.strftime("%Y-%m-%dT%H:%M")
     data, _ = get_point_forecast(lat, lng, time, model=model, timezone=timezone)
     if data is None:
         return err("No data available for the specified time.")
+    data["timezone"] = f'{"+" if timezone >= 0 else ""}{timezone:02d}:00'
+    data["mainCloudBottom"] = find_main_cloud_bottom(data, main_cloud_rate_to_max_cc, skip_low_clouds_threshold)[0]
     return ok(data)
